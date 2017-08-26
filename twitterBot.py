72aface27992bd71ba17bcfe7e9914d948aef903
Henry Xu <henryxu@berkeley.edu>
2017-08-25 23:06:25 -0700

    cleaned up file system

--- twitterBot.py
+++ twitterBot.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from threading import Timer
 import tweepy
-import getquote
+import getQuote
+print(getQuote.get_quote())
+x = datetime.today()
 
- x = datetime.today()
-
- y = x.replace(day=x.day+1, hour=12, minute=30, second=0, microsecond=0)
- delta_t = y - x
+y = x.replace(day=x.day+1, hour=12, minute=30, second=0, microsecond=0)
+delta_t = y - x
 
 secs = delta_t.seconds + 1
 
@@ -16,7 +16,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 def quote_of_the_day():
-    api.update_status(status = get-quote.get_quote())
+    api.update_status(status = getQuote.get_quote())
 
 t = Timer(secs, quote_of_the_day)
 t.start()
